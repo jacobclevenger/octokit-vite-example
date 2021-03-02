3c81b1fc24c03a0d79e4be98435ebed3fc7dc343
Jacob Clevenger <jacobrclevenger@gmail.com>
2021-03-02 18:25:15 -0500

    init commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { Octokit } from 'https://cdn.skypack.dev/@octokit/rest'
+
+console.log(new Octokit())
+</script>
+
+<template>
+  <div>
+    Hello World
+  </div>
+</template>
---
+++ src/shims.d.ts
@@ -0,0 +1,5 @@
+declare module 'https://cdn.skypack.dev/@octokit/rest' {
+  import { Octokit, RestEndpointMethodTypes } from '@octokit/rest'
+
+  export { Octokit, RestEndpointMethodTypes }
+}
